b0043a7be5ee51831246c6b6450b1cf9f3af93b0
litvinovjobs@gmail.com <litvinovjobs@gmail.com>
2023-01-26 22:35:36 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+// Task 1
+
+// let userName = prompt("Введіть будь ласка ваше імя: ");
+// console.log("Вітаю, вас звати: ", userName);
+// alert(`Вітаю, вас зват ${userName}`)
+
+
+// Task 2
+
+// let numberYear = +prompt("Введіть рік вашого народження: ");
+// const year = 2023;
+// let result = year - numberYear;
+// alert(`Вітаю, вам ${result} років:)`)
+
+
+// Task 3
+
+// let lengthSquare = +prompt("Введіть будь ласка довжину сторони квадрата:");
+// let result = lengthSquare * 4;
+// alert(`Периметр квадрата вказаной вами довжини буде: ${result}`);
+
+
+// Task 4
+
+// let circleRadius = +prompt("Введіть будь ласка радіус кола: ");
+// const pi = 3.14;
+// let result = (circleRadius * circleRadius) * pi;
+// alert(`Площа вашого кругу: ${result} см`);
+
+
+// Task 5
+
+// let question1 = +prompt("Введіть відстань між містами у які ви хочете попасти: ");
+// let question2 = +prompt("За скільки часу ви туди хочете потрапити?");
+// let result = ~~(question1 / question2);
+// alert(`${result}км \nЗ такою швидкість вам потрібно їхати, щоб потрапити у ваше місце за стільки годин ${question2} `);
+
+
+// Task 6
+
+// let summDolar = +prompt("Введіть суму валюти: ");
+// const euro = 1.082;
+// let result = (summDolar / euro);
+// alert(`Сума в євро буде ${result}`);
+
+
+// Task 7
+
+// let flashMemory = +prompt("Введіть обсяг флешки в ГБ: ");
+// flashMemory = flashMemory * 1000;
+// let file = 820;
+// let result = ~~(flashMemory / 820);
+
+// alert(`Кількість файлів що вміститься на вашій флешці: ${result}`);
+
+
+// Task 8
+
+// let summClient = +prompt("Введіть вашу суму: ");
+// let priceChocolate = +prompt("Введіть вартість шоколадки: ");
+// let result = ~~(summClient / priceChocolate);
+// let formula = summClient - (result * priceChocolate);
+
+// alert(`Ви зможете купити ${result} шоколадок і отримаєте ${formula}грн здачі`);
